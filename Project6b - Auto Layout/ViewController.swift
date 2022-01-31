33b177147457c98974cf1d1dc88b99d00a8300ee
John Kim <cloudiosx@gmail.com>
2022-01-31 13:52:06 -0500

    first commit

---
+++ Project6b - Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b - Auto Layout
+//
+//  Created by John Kim on 1/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
