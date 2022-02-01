900b9df956dad4ad7ce9d5df7a1fac670634d526
John Kim <cloudiosx@gmail.com>
2022-02-01 10:14:37 -0500

    Add Auto Layout anchors

--- Project6b - Auto Layout/ViewController.swift
+++ Project6b - Auto Layout/ViewController.swift
@@ -51,6 +51,7 @@ class ViewController: UIViewController {
         
         // Auto Layout Visual Format Language
         
+        /*
         let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         let metrics = ["labelHeight": 88]
         
@@ -60,6 +61,26 @@ class ViewController: UIViewController {
         
 //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        */
+        
+        // Auto Layout Anchors
+        
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                // We have a previous label - create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            // Set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
         
 //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label1]|", options: [], metrics: nil, views: viewsDictionary))
 //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label2]|", options: [], metrics: nil, views: viewsDictionary))
